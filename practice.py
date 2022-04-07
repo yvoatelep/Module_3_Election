52882efd3cbe0efda6a17c236bab5728b9e6dbea
yvoatelep <yvoatelep@gmail.com>
2022-04-07 09:42:32 -0600

    this is a test commit

---
+++ practice.py
@@ -0,0 +1,8 @@
+# print("Hello World")
+# type (3)
+# num = 5+2*3
+# print (num)
+# num =8//5-3
+# print (num)
+# print (type (3))
+print (8+22*2-4)
---
+++ test2.py
@@ -0,0 +1,10 @@
+from unittest.util import _count_diff_all_purpose
+
+
+print("election results")
+counties=["Arapahoe", "Denver", "Jefferson", "Boulder"]
+print (counties)
+print (counties)
+counties. append ("Weld")
+Print ("Hello World!") 
+
