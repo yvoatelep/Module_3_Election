4e8db5b61ea139adcf78cc5c1afb8fbe4bd2b165
yvoatelep <yvoatelep@gmail.com>
2022-04-09 20:24:48 -0600

    script running after module 354

--- practice.py
+++ practice.py
@@ -5,4 +5,161 @@
 # num =8//5-3
 # print (num)
 # print (type (3))
-print (8+22*2-4)
+# print(8+22*2-4)
+# num =8+(22*(2-4))
+# print(num)
+# print (16-3/(2+7)-1)
+# counties = ["Arapahoe","Denver", "Jefferson"]
+# counties. append("Boulder")
+# print (counties)
+# counties.insert(2, "Weld")
+# print (counties) 
+# counties.pop(3)
+# print(counties) 
+# counties_tuple = ("Arapahoe", "Denver", "Boulder")
+# len(counties_tuple)
+# print (counties_tuple)
+# counties_dict ={}
+# counties_dict ["Arapahoe"] = 422829
+# print (counties_dict)
+# counties_dict ["Denver"]= 463353
+# counties_dict ["Jefferson"]=432438
+# # print (counties_dict)
+# len(counties_dict)
+# voting_data =[]
+# voting_data.append({"county":"Arapahoe","registered_voters": 422829})
+# voting_data.append({"county":"Denver", "registered_voters": 463353})
+# voting_data.append({"county":"Jefferson","registered_voters": 432438})
+# print (voting_data)
+# voting_data.insert(1,{"county":"El Paso","registered_voters":461149})
+# print (voting_data)
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if counties [1] =='Denver':
+#     print (counties[1])
+# if len (counties) >2: 
+#     print (counties)
+# Temperature = int(input("What is the temperature outside>"))
+
+# if temperature >= 80:
+#     print("Turn on the AC.")
+# else:
+#     print("Open the windows.")
+#What is the score?
+# score = int(input("What is your test score?")) 
+
+# #Determine the grade.
+# if score >= 90:
+#     print('Your grade is an A.')
+# else:
+#     if score >= 80:
+#         print('Your grade is an B.')
+#     else:
+#         if score >= 70:
+#             print('Your grade is an C.')
+#         else:
+#             if score >= 60:
+#                 print('Your grade is an.')
+#             else:
+#                 print ('Your grade is an F.')
+#What is the score? revamped
+#score = int(input("What is your test score?")) 
+
+#Determine the grade.
+# if score >= 90:
+#     print('Your grade is an A.')
+
+# elif score >= 80:
+#     print('Your grade is a B.')
+
+# elif score >= 70:
+#     print('Your grade is a C.')
+
+# elif score >= 60:
+#     print('Your grade is a D.')
+
+# else:
+#     print('Your grade is an F.')
+#counties = ["Arapahoe","Denver", "Jefferson"]
+# if "El Paso" in counties:
+#     print ("Yes.") 
+# else:
+#     print("No")
+# for county in counties:
+#     print (county)
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+# for county in counties_dict.keys():
+#         print (county)
+# for voters in counties_dict.values():
+#         print(voters)
+# for county, voters in counties_dict.items():
+#     print(f"(county)has(voters) voters)
+
+# my_votes = int(input("How many votes did you get in the election? "))
+# total_votes = int(input("What is the total votes in the election? "))
+# print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+
+# for county, voters in counties_dict.items():
+#     print(f"{county} county has {voters} registered voters.")
+
+# candidate_votes = int(input("How many votes did the candidate get in the election? "))
+# total_votes = int(input("What is the total number of votes in the election? "))
+# message_to_candidate = (
+#     f"You received {candidate_votes} number of votes. "
+#     f"The total number of votes in the election was {total_votes}. "
+#     f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+
+# print(message_to_candidate)
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+print (f"{county} has{voters} registered voters")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
